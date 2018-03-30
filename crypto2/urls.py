aabd4051f91755f4f6a40d6da6b0ca3e0bca807a
Noah Sung <noahcs2@illinois.edu>
2018-03-29 20:24:15 -0500

    Linked views.py to separate html file

--- crypto2/urls.py
+++ crypto2/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-	path('exchange', views.exchange_currency, name = 'exchange'),
+	path('<str:currency>_<int:amount>/', views.exchange_currency, name = 'exchange'),
 ]
--- crypto2/views.py
+++ crypto2/views.py
@@ -2,17 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 from . import exchange
 
 def index(request):
+	"""
 	button = "<button class=""button"">Submit</button>"
 	input_curr = "Currency: <input type=""text"" name=""currency"">"
 	input_amount = "Amount: <input type=""text"" name=""amount"">"
 	body = "<body>" + "<form>" + input_curr + "<br>" + input_amount + "<br>" + button + "</form>" + "</body>"
 	response = "<!DOCTYPE html><html><title></title><head><h1>Cryptocurrency Exchange Simulation</h1></head>" + body + "</html>"
-	return HttpResponse(response)
+	"""
+	template = loader.get_template('crypto2/index.html')
+	context = {}
+	return HttpResponse(template.render(context, request))
 
-def exchange_currency(request):
+def exchange_currency(request, currency, amount):
 	currency = "USD"
 	amount = 10
 	binance = exchange.Exchange("binance")

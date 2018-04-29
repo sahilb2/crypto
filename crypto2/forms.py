769114c22d7d4a7d7dba2268628a6b1237b57fed
Noah Sung <noahcs2@illinois.edu>
2018-04-29 14:11:37 -0500

    updated

--- crypto2/forms.py
+++ crypto2/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class SimulationForm(forms.Form):
-	amount = forms.IntegerField()
+	amount = forms.FloatField()
 	currency = forms.CharField()
-	duration = forms.IntegerField()
+	duration = forms.FloatField()
